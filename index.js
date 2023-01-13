901f60a54162fb40644e52eb1f36b9a1c831c5b2
Checkmate990 <108345290+Checkmate990@users.noreply.github.com>
2023-01-13 11:29:08 -0700

    Update index.js
    Easy

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride=''){
+  if (ride<= 400){
+    return 'This one is on me!';
+  }else if (ride<= 2000 && ride>400){
+    return 'That will be twenty bucks.';
+  }else if (ride>2000 && ride<= 2499){
+    return 'I will gladly take your thirty bucks.';
+  }else if (ride>=2500){
+    return 'No can do.';
+  }
+}  
+
+function ternaryCheckCity(city=''){
+  if( city === 'NYC') {
+      return 'Ok, sounds good.'  
+  } else if ( city != 'NYC')  {
+    return 'No go.' 
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+  function switchOnCharmFromTip(trip=''){
+    if (trip === 'generous') {
+      return 'Thank you so much.'
+    } else if (trip === 'not as generous') {
+      return 'Thank you.'
+    } else if (trip === 'thanks for everything') {
+      return 'Bye.'
+  
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
